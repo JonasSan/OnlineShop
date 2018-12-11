cadabe020ec92c73ed8a8b455f87c1b75a59d337
ninalstrp <nina.lestrup@gmail.com>
2018-12-11 13:46:09 +0100

    first commit

---
+++ src/main/java/com/example/vidajoni/VidajoniApplication.java
@@ -0,0 +1,12 @@
+package com.example.vidajoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidajoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidajoniApplication.class, args);
+	}
+}
